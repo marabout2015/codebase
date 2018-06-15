ec905493f6b708162613e65cc3ec537446290a88
fboylu <fboylu@microsoft.com>
2018-06-15 19:02:03 +0000

    CosmosDB notebooks

---
+++ CosmosDB/config.py
@@ -0,0 +1,7 @@
+settings = {
+    'end_point': 'https://xxxxxxxxxx.documents.azure.com:443/',
+    'master_key': 'xxxxxxxxxxx',
+    'master_key_alt': 'xxxxxxxxxxxx',
+    'database_id': 'xxxxx',
+    'collection_id': 'xxxxxx'
+}
